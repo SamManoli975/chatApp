642f59bbd6bf42d688341fb6f9a6c47fc8c11ed3
SamManoli975 <C23330671@mytudublin.ie>
2024-04-02 16:07:06 +0100

    added client side and server side

---
+++ client/app/page.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { io, Socket } from 'socket.io-client'; // Import 'io' and 'Socket' from 'socket.io-client'
+import { useEffect, useState } from "react";
+
+const socket: Socket = io("http://localhost:3002"); // Use 'io' to create a socket connection
+
+export default function Home() {
+  return (
+    <main>
+      <div>hi</div>
+    </main>
+  );
+}
+
