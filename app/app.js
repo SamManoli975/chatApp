3d31c1afebe97fccdf15f5e017751027902d60b9
SamManoli975 <C23330671@mytudublin.ie>
2024-05-19 22:31:08 +0100

    fixed a small bug and now the 2 sided server real time chat is working bare minimum

--- app/app.js
+++ app/app.js
@@ -1,10 +1,9 @@
-const socket = io('ws://localhost:3000')
+const socket = io('ws://localhost:3500')
 
-
-function sendMessage(e){
+function sendMessage(e) {
     e.preventDefault()
     const input = document.querySelector('input')
-    if(input.value){
+    if (input.value) {
         socket.emit('message', input.value)
         input.value = ""
     }
@@ -12,12 +11,11 @@ function sendMessage(e){
 }
 
 document.querySelector('form')
-    .addEventListener('submit',sendMessage)
+    .addEventListener('submit', sendMessage)
 
-//listen
+// Listen for messages 
 socket.on("message", (data) => {
     const li = document.createElement('li')
     li.textContent = data
     document.querySelector('ul').appendChild(li)
 })
-
