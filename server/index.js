541a2819864a1f4585fd15597ec232425cdb1aaf
SamManoli975 <C23330671@mytudublin.ie>
2024-05-20 21:16:57 +0100

    changes

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
-app.use(express.static(path.join(__dirname)))
+app.use(express.static(path.join(__dirname, "public")))
 
 const expressServer = app.listen(PORT, () => {console.log       (`listening on port ${PORT}`)
 })
