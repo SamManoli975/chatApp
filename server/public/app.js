5a29e3c999a61fc108f9bdaf0a35efe98a5a1b0b
SamManoli975 <C23330671@mytudublin.ie>
2024-05-23 16:18:27 +0100

    starting activity detection

--- server/public/app.js
+++ server/public/app.js
@@ -1,13 +1,16 @@
 const socket = io('ws://localhost:3000')
 
+const activity = document.querySelector('.activity')
+const msgInput = document.querySelector('input')
+
 function sendMessage(e) {
     e.preventDefault()
-    const input = document.querySelector('input')
-    if (input.value) {
-        socket.emit('message', input.value)
-        input.value = ""
+    
+    if (msgInput.value) {
+        socket.emit('message', msgInput.value)
+        msgInput.value = ""
     }
-    input.focus()
+    msgInput.focus()
 }
 
 document.querySelector('form')
@@ -18,4 +21,8 @@ socket.on("message", (data) => {
     const li = document.createElement('li')
     li.textContent = data
     document.querySelector('ul').appendChild(li)
+})
+
+msgInput.addEventListener('keypress', () => {
+    socket.emit('activity', socket.id.substring(0, 5))
 })
